c37b0a39aa9e4bac303f1f9e4cf1f61e7a650eb7
GitJaeHyeon <wogusworksp@gmail.com>
2025-08-04 23:16:19 +0900

    8월 4일 월 Toss Project
    8월 2일 토 13일차
    - 게임 오버 다시하기 / 이어하기 기능 추가
    
    8월 3~4일 일~월 14~15일차
    - 게임 대기 UI 구현
    - TakeDamage 실행안되던 버그 수정
    - 사운드 트랙 서칭

--- Assets/2. Script/Manager/GameManager.cs
+++ Assets/2. Script/Manager/GameManager.cs
@@ -5,14 +5,18 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    private PlayerStats playerStats;
+    
+    [Header("상태 바")]
     public TextMeshProUGUI playTimeUI;
     public TextMeshProUGUI scoreUI;
-
     public Slider slid_Score;
     public Slider slid_Time;
     
+    [Header("화면 전환")]
     public GameObject gameoverUI;
     public GameObject stageclearUI;
+    public GameObject beforestartUI;
 
     private static float setTime = 60f;
     private static float currTime;
@@ -22,13 +26,27 @@ public class GameManager : MonoBehaviour
     
     public static bool isPlay;
 
-    void Start()
+    void OnEnable()
     {
-        currScore = 0;
-        
-        currTime = setTime;
+        Time.timeScale = 0f;
+        isPlay = false;
+    
+        beforestartUI.SetActive(true);  // 게임 시작 전 UI 보여줌
+    }
+
+    void StartGame()
+    {
+        beforestartUI.SetActive(false);
         isPlay = true;
         Time.timeScale = 1f;
+    }
+
+    void Start()
+    {
+        playerStats = FindObjectOfType<PlayerStats>();
+        
+        currScore = 0;
+        currTime = setTime;
 
         slid_Score.maxValue = targetScore;
         slid_Score.value = targetScore - currScore;
@@ -39,7 +57,19 @@ public class GameManager : MonoBehaviour
     
     void Update()
     {
-        if (!isPlay) return;
+        // 게임 시작 전, 터치로 시작
+        if (!isPlay)
+        {
+#if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBGL
+            if (Input.GetMouseButtonDown(0))
+#else
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+#endif
+            {
+                StartGame();
+            }
+            return;
+        }
 
         // 시간 감소 먼저!
         currTime -= Time.deltaTime;
@@ -55,15 +85,15 @@ public class GameManager : MonoBehaviour
         // ✅ 슬라이더 갱신 순서를 여기서!
         slid_Score.value = targetScore - currScore;
         slid_Time.value = currTime;
-
-        if (currTime <= 0f)
+        
+        if (currTime <= 0f || playerStats.hp <= 0f)
         {
             gameoverUI.gameObject.SetActive(true);
             isPlay = false;
             Time.timeScale = 0f;
         }
 
-        if (currScore == targetScore)
+        if (currScore >= targetScore)
         {
             stageclearUI.gameObject.SetActive(true);
             isPlay = false;
@@ -81,13 +111,8 @@ public class GameManager : MonoBehaviour
         gameoverUI.gameObject.SetActive(false);
         isPlay = true;
 
+        playerStats.hp += 2f;
         currTime += 30f;
         Time.timeScale = 1f;
-    }
-
-    public static void ResetPlayUI()
-    {
-        currScore = 0;
-        setTime = 15f;
     }
 }
